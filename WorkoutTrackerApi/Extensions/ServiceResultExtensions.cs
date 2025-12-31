a7ea8f8e82d076064cb5b278072bafafe30c8e57
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-31 03:12:37 +0100

    feat(extensions): Add ServiceResultExtensions for standardized action result conversion

---
+++ WorkoutTrackerApi/Extensions/ServiceResultExtensions.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using WorkoutTrackerApi.DTO.Global;
+using WorkoutTrackerApi.Services.Results;
+
+namespace WorkoutTrackerApi.Extensions;
+
+public static class ServiceResultExtensions
+{
+    public static IActionResult ToActionResult(this ServiceResult result, string successMessage = "Success")
+    {
+        if (result.IsSucceeded)
+        {
+            return new OkObjectResult(ApiResponse.Success(successMessage));
+        }
+
+        return new ObjectResult(ApiResponse.Failure("An error occurred", result.Errors.First()));
+    }
+    
+    public static IActionResult ToActionResult<T>(this ServiceResult<T> result, string successMessage = "Success")
+    {
+        if (result.IsSucceeded)
+        {
+            return new OkObjectResult(ApiResponse<T>.Success(successMessage, result.Payload!));
+
+        }
+
+        return new ObjectResult(ApiResponse.Failure("An error occurred", result.Errors.First()));
+    }
+}

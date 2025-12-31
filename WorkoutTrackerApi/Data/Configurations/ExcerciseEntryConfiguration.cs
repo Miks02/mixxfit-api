58a62df1b21b7149eea0fa586428c17b752214d2
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-31 15:10:39 +0100

    feat(config): Extend ExerciseEntryConfiguration class

--- WorkoutTrackerApi/Data/Configurations/ExcerciseEntryConfiguration.cs
+++ WorkoutTrackerApi/Data/Configurations/ExcerciseEntryConfiguration.cs
@@ -13,9 +13,21 @@ public class ExerciseEntryConfiguration : IEntityTypeConfiguration<ExerciseEntry
 
         builder.HasIndex(p => p.Name);
 
-        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.DistanceKm)}_Positive", "DistanceKm > 0"));
         builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.AvgHeartRate)}_Positive", "AvgHeartRate > 0"));
-        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.CaloriesBurned)}_Positive", "CaloriesBurned > 0"));
+        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.MaxHeartRate)}_Positive", "MaxHeartRate > 0"));
+        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.MaxHeartRate)}_GreaterThan_AvgHeartRate", "MaxHeartRate >= AvgHeartRate"));
+        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.WorkIntervalSec)}_Positive", "WorkIntervalSec > 0"));
+        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.RestIntervalSec)}_Positive", "RestIntervalSec > 0"));
+        builder.ToTable(entries => entries.HasCheckConstraint($"CK_{nameof(ExerciseEntry)}s_{nameof(ExerciseEntry.IntervalsCount)}_Positive", "IntervalsCount > 0"));
+
+        builder.Property(p => p.DistanceKm)
+            .HasPrecision(5, 2);
+
+        builder.Property(p => p.CaloriesBurned)
+            .HasPrecision(7, 2);
+
+        builder.Property(p => p.PaceMinPerKm)
+            .HasPrecision(5, 2);
 
         builder
             .HasMany(e => e.Sets)

48511e53528e4db3cbab420910010fcb47b6b909
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-07 22:30:37 +0100

    feat(dto): Add TotalCountPaginated property in PagedResult and add WorkoutCount property to the WorkoutSummaryDto

--- WorkoutTrackerApi/DTO/Global/PagedResult.cs
+++ WorkoutTrackerApi/DTO/Global/PagedResult.cs
@@ -5,9 +5,10 @@ public class PagedResult<T> where T : class
     public IReadOnlyList<T> Items { get; set; }
     public int Page { get; set; }
     public int PageSize { get; set; }
+    public int TotalCountPaginated { get; set; }
     public int TotalCount { get; set; }
 
-    public PagedResult(IReadOnlyList<T> items, int page, int pageSize, int totalCount)
+    public PagedResult(IReadOnlyList<T> items, int page, int pageSize, int totalCountPaginated, int totalCount)
     {
         if (page < 1)
             throw new ArgumentException("Page cannot be 0 or have negative value");
@@ -17,6 +18,7 @@ public class PagedResult<T> where T : class
         Items = items;
         Page = page;
         PageSize = pageSize;
+        TotalCountPaginated = totalCountPaginated;
         TotalCount = totalCount;
     }
     
--- WorkoutTrackerApi/DTO/Workout/WorkoutSummaryDto.cs
+++ WorkoutTrackerApi/DTO/Workout/WorkoutSummaryDto.cs
@@ -5,9 +5,8 @@ namespace WorkoutTrackerApi.DTO.Workout;
 
 public class WorkoutSummaryDto
 {
+    public int WorkoutCount { get; set; }
     public int ExerciseCount { get; set; }
-
     public DateTime? LastWorkoutDate { get; set; }
-
     public ExerciseType FavoriteExerciseType { get; set; }
 }

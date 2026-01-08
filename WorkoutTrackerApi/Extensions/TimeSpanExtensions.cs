7fe9f2f57012158b615f36163aba67810856ff12
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-08 13:44:08 +0100

    feat(extensions): Add TimeSpanExtension static class that has methods for validating nullable TimeSpan objects

---
+++ WorkoutTrackerApi/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkoutTrackerApi.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static int? ToIntegerFromNullableSeconds(this TimeSpan? timeSpan)
+        {
+            if (timeSpan is null)
+                return null;
+
+            return timeSpan.Value.Seconds;
+
+        }
+
+        public static int? ToIntegerFromNullableMinutes(this TimeSpan? timeSpan)
+        {
+            if (timeSpan is null)
+                return null;
+            return (int)timeSpan.Value.TotalMinutes;
+        }
+        
+    }
+}

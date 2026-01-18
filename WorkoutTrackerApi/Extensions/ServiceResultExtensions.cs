657092b0f14a6da691a1bb921f866427ea7b371a
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-18 13:05:20 +0100

    feat(extension): Remove ToIActionResult extension methods

--- WorkoutTrackerApi/Extensions/ServiceResultExtensions.cs
+++ WorkoutTrackerApi/Extensions/ServiceResultExtensions.cs
@@ -6,26 +6,6 @@ namespace WorkoutTrackerApi.Extensions;
 
 public static class ServiceResultExtensions
 {
-    public static IActionResult ToIActionResult(this ServiceResult result, string successMessage = "Success")
-    {
-        if (result.IsSucceeded)
-        {
-            return new OkObjectResult(ApiResponse.Success(successMessage));
-        }
-
-        return new ObjectResult(ApiResponse.Failure(result.Errors.First()));
-    }
-    
-    public static IActionResult ToIActionResult<T>(this ServiceResult<T> result, string successMessage = "Success")
-    {
-        if (result.IsSucceeded)
-        {
-            return new OkObjectResult(ApiResponse<T>.Success(successMessage, result.Payload!));
-
-        }
-
-        return new ObjectResult(ApiResponse.Failure(result.Errors.First()));
-    }
 
     public static ActionResult ToActionResult(this ServiceResult result, string successMessage = "Success")
     {

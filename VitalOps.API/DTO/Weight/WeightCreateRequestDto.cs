6b7e5eb4f3e5b0e69f0081be3795f4f780f25123
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-21 19:10:11 +0100

    feat(dto): Add multiple dto's related to weight entires
    - WeightCreateRequestDto
    - WeightEntryDetailsDto
    - WeightSummaryDto

---
+++ VitalOps.API/DTO/Weight/WeightCreateRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VitalOps.API.DTO.Weight
+{
+    public class WeightCreateRequestDto
+    {
+        public double Weight { get; set; }
+        public TimeSpan Time { get; set; }
+        public string? Notes { get; set; }
+    }
+}
---
+++ VitalOps.API/DTO/Weight/WeightSummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VitalOps.API.DTO.Weight
+{
+    public class WeightSummaryDto
+    {
+        public double CurrentWeight { get; set; }
+        public double Progress { get; set; }
+        public IReadOnlyList<WeightEntryDetailsDto> WeightEntries { get; set; } = new List<WeightEntryDetailsDto>();
+    }
+}

3bb66a763234619df7578000bf2ce252a01c19ea
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-27 05:37:48 +0100

    feat(dto): Add Id and TimeLogged to WeightRecordDto and switch WeightEntries to WeightLogs in WeightSummaryDto

--- VitalOps.API/DTO/Weight/WeightRecordDto.cs
+++ VitalOps.API/DTO/Weight/WeightRecordDto.cs
@@ -2,7 +2,9 @@
 {
     public class WeightRecordDto
     {
+        public int Id { get; set; }
         public double Weight { get; set; }
+        public TimeSpan TimeLogged { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
--- VitalOps.API/DTO/Weight/WeightSummaryDto.cs
+++ VitalOps.API/DTO/Weight/WeightSummaryDto.cs
@@ -1,10 +1,13 @@
-﻿namespace VitalOps.API.DTO.Weight
+﻿using VitalOps.API.DTO.Global;
+
+namespace VitalOps.API.DTO.Weight
 {
     public class WeightSummaryDto
     {
         public WeightRecordDto FirstEntry { get; set; } = null!;
         public WeightRecordDto CurrentWeight { get; set; } = null!;
         public double Progress { get; set; }
-        public IReadOnlyList<WeightEntryDetailsDto> WeightEntries { get; set; } = new List<WeightEntryDetailsDto>();
+        public IReadOnlyList<WeightRecordDto> WeightLogs { get; set; } = [];
+
     }
 }

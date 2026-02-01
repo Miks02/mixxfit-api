ba9f17f8f101afad7b89ddbf0188303d4f0a18a9
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-02-01 23:34:07 +0100

    feat(service): Add and implement IFileService interface
    - Implement methods UploadFile and DeleteFile

---
+++ VitalOps.API/Services/Interfaces/IFileService.cs
@@ -0,0 +1,10 @@
+﻿using VitalOps.API.Services.Results;
+
+namespace VitalOps.API.Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<Result<string>> UploadFile(IFormFile file, string? uploadedFilePath, string? uploadSubDir);
+        void DeleteFile(string fileToDeletePath);
+    }
+}

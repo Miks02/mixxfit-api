a7d2465a09cab9c2d733777c8436a96bde23625b
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-02-05 14:33:19 +0100

    feat(validation): Add upper limits for reps and weight in SetEntryDtoValidator

--- MixxFit.API/Validators/SetEntryDtoValidator.cs
+++ MixxFit.API/Validators/SetEntryDtoValidator.cs
@@ -9,10 +9,14 @@ public class SetEntryDtoValidator : AbstractValidator<SetEntryDto>
     {
         RuleFor(p => p.Reps)
             .GreaterThan(0)
-            .WithMessage("At least 1 repetition is required");
+            .WithMessage("At least 1 repetition is required")
+            .LessThan(1001)
+            .WithMessage("Number of reps can be between 1 and 1000");
 
         RuleFor(p => p.WeightKg)
             .GreaterThan(0)
-            .WithMessage("Weight is required");
+            .WithMessage("Weight is required")
+            .LessThan(1001)
+            .WithMessage("Amount of weight can be between 1 and 1000 kg's");
     }
 }

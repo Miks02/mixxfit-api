440db8e4fbc0eb03493c184cdf8cafca73d03e3b
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-02-24 10:58:57 +0100

    feat(extension): Add TimeSpan extension methods

--- MixxFit.VSA/Common/Extensions/UtilityExtensions.cs
+++ MixxFit.VSA/Common/Extensions/UtilityExtensions.cs
@@ -3,4 +3,18 @@ namespace MixxFit.VSA.Common.Extensions;
 public static class UtilityExtensions
 {
     public static string ToLowerFirstLetter(this string str) => str.Substring(0, 1).ToLower() + str.Substring(1);
+    
+    public static int? ToIntegerFromNullableSeconds(this TimeSpan? timeSpan)
+    {
+        if (timeSpan is null)
+            return null;
+        return timeSpan.Value.Seconds;
+    }
+
+    public static int? ToIntegerFromNullableMinutes(this TimeSpan? timeSpan)
+    {
+        if (timeSpan is null)
+            return null;
+        return (int)timeSpan.Value.TotalMinutes;
+    }
 }

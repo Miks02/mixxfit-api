dcde3a3f03fc9aa45159618927767d89d88e10b1
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-04 18:16:48 +0100

    refactor(validator): Remove 'NotEmpty' validation rule for ActivityLevel enum

--- MixxFit.VSA/Features/Nutrition/CalculateCalories/CalculateCaloriesValidator.cs
+++ MixxFit.VSA/Features/Nutrition/CalculateCalories/CalculateCaloriesValidator.cs
@@ -11,28 +11,26 @@ public class CalculateCaloriesValidator : AbstractValidator<CalculateCaloriesReq
             .WithMessage("Age is required")
             .InclusiveBetween(13, 130)
             .WithMessage("Age must be between 13 and 130 years old.");
-        
+
         RuleFor(x => x.Gender)
             .NotEmpty()
             .WithMessage("Gender is required")
             .IsInEnum()
             .WithMessage("Invalid gender enum.");
-        
+
         RuleFor(x => x.Height)
             .NotEmpty()
             .WithMessage("Height is required")
             .InclusiveBetween(70, 250)
             .WithMessage("Height must be between 70 and 250 cm.");
-        
+
         RuleFor(x => x.Weight)
             .NotEmpty()
             .WithMessage("Weight is required")
             .InclusiveBetween(25, 400)
             .WithMessage("Weight must be between 25 and 400 kg.");
-        
+
         RuleFor(x => x.ActivityLevel)
-            .NotEmpty()
-            .WithMessage("Activity level is required")
             .IsInEnum()
             .WithMessage("Invalid activity level enum.");
     }

82c1e68ec88a9cc60c452c409cd8266154548e55
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-16 13:50:56 +0100

    chore(migrations): Add seed data for MuscleGroups and ExerciseCategories and commit migrations
    - Inserted initial seed data for MuscleGroups and ExerciseCategories tables.
    - Added corresponding Up and Down migration logic.

--- MixxFit.API/Infrastructure/Persistence/Configurations/ExerciseCategoryConfiguration.cs
+++ MixxFit.API/Infrastructure/Persistence/Configurations/ExerciseCategoryConfiguration.cs
@@ -19,5 +19,19 @@ public class ExerciseCategoryConfiguration : IEntityTypeConfiguration<ExerciseCa
         
         builder.HasIndex(p => p.Name)
             .IsUnique();
+        
+        builder.HasData(
+            new ExerciseCategory { Id = 1, Name = "Cardio" },
+            new ExerciseCategory { Id = 2, Name = "Barbell" },
+            new ExerciseCategory { Id = 3, Name = "Dumbbell" },
+            new ExerciseCategory { Id = 4, Name = "Machine" },
+            new ExerciseCategory { Id = 5, Name = "Other" },
+            new ExerciseCategory { Id = 6, Name = "Bodyweight" },
+            new ExerciseCategory { Id = 7, Name = "Duration" },
+            new ExerciseCategory { Id = 8, Name = "Assisted Bodyweight" },
+            new ExerciseCategory { Id = 9, Name = "Stretching" },
+            new ExerciseCategory { Id = 10, Name = "Olympic" },
+            new ExerciseCategory { Id = 11, Name = "Cable" }
+            );
     }
 }
--- MixxFit.API/Infrastructure/Persistence/Configurations/MuscleGroupConfiguration.cs
+++ MixxFit.API/Infrastructure/Persistence/Configurations/MuscleGroupConfiguration.cs
@@ -19,5 +19,17 @@ public class MuscleGroupConfiguration : IEntityTypeConfiguration<MuscleGroup>
             .WithOne(e => e.MuscleGroup)
             .HasForeignKey(e => e.MuscleGroupId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasData(
+            new MuscleGroup { Id = 1, Name = "Chest" },
+            new MuscleGroup { Id = 2, Name = "Back" },
+            new MuscleGroup { Id = 3, Name = "Shoulders" },
+            new MuscleGroup { Id = 4, Name = "Arms" },
+            new MuscleGroup { Id = 5, Name = "Legs" },
+            new MuscleGroup { Id = 6, Name = "Core" },
+            new MuscleGroup { Id = 7, Name = "Glutes" },
+            new MuscleGroup { Id = 8, Name = "Full Body" },
+            new MuscleGroup { Id = 9, Name = "Other" }
+        );
     }
 }

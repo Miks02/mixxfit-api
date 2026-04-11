815dcdbc8e8a5ad7495a81d165f0071401b09d15
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-04-11 12:01:49 +0200

    feat(extensions): Add various hangfire extensions methods that will be used for app configuration and DI

---
+++ MixxFit.API/Infrastructure/Hangfire/HangfireExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Hangfire;
+using Hangfire.Dashboard;
+using Hangfire.PostgreSql;
+using MixxFit.API.Features.Exercises.CleanUpExercisesJob;
+
+namespace MixxFit.API.Infrastructure.Hangfire
+{
+    public static class HangfireExtensions
+    {
+
+        public static void AddHangfireSetup(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHangfire((sp, config) =>
+            {
+                config.UsePostgreSqlStorage(options => options.UseNpgsqlConnection(configuration.GetConnectionString("PostgresConnection")));
+            });
+            services.AddHangfireServer();
+        }
+
+        public static void AddRecurringJobs(this IServiceCollection services)
+            => services.AddScoped<CleanUpExercisesJob>();
+     
+        public static void UseHangfireDashboardWithAuthorization(this WebApplication app)
+        {
+            var config = app.Configuration;
+            var user = config["Hangfire:Username"] ?? "";
+            var password = config["Hangfire:Password"] ?? "";
+
+            if (app.Environment.IsProduction() && (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(password)))
+                throw new InvalidOperationException("Hangfire credentials are not set");
+
+            IDashboardAuthorizationFilter authFilter = app.Environment.IsDevelopment() 
+                ? new HangfireAnonymousFilter()
+                : new HangfireAuthorizationFilter(user, password);
+
+            var options = new DashboardOptions
+            {
+                Authorization = [authFilter]
+            };
+            app.UseHangfireDashboard("/hangfire", options);
+        }
+
+        public static void UseRecurringJobs(this WebApplication app)
+        {
+            RecurringJob.AddOrUpdate<CleanUpExercisesJob>(
+                "cleanup-exercises",
+                job => job.Execute(),
+                Cron.Daily(0));
+
+        }
+    }
+}

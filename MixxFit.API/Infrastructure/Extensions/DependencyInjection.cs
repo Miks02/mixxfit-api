a0033e4c6640a8e858c9967f6444714f85b4a22c
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-04-11 19:24:48 +0200

    refactor(program.cs): Move  all infrastructure related service registration into the AddInfrastructure extension method
    - Cleaned up program.cs file to make it as simple as possible while improving readability

--- MixxFit.API/Infrastructure/Extensions/DependencyInjection.cs
+++ MixxFit.API/Infrastructure/Extensions/DependencyInjection.cs
@@ -1,9 +1,35 @@
+using FluentValidation;
 using MixxFit.API.Common.Interfaces;
+using MixxFit.API.Features.Workouts.CreateWorkout;
+using MixxFit.API.Infrastructure.Cloudinary;
+using MixxFit.API.Infrastructure.Cors;
+using MixxFit.API.Infrastructure.Exceptions;
+using MixxFit.API.Infrastructure.Hangfire;
+using MixxFit.API.Infrastructure.Persistence;
+using MixxFit.API.Infrastructure.RateLimiting;
+using MixxFit.API.Infrastructure.Security;
 
 namespace MixxFit.API.Infrastructure.Extensions;
 
 public static class DependencyInjection
 {
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddPersistence(configuration);
+        services.AddSecurity(configuration);
+        services.AddHttpContextAccessor();
+        services.AddCloudinary(configuration);
+        services.AddHangfireSetup(configuration);
+        services.AddRecurringJobs();
+        services.AddProblemDetails();
+        services.AddValidatorsFromAssemblyContaining<Program>(filter: descriptor => descriptor.ValidatorType != typeof(SetEntryValidator));
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddOpenApi();
+        services.InjectHandlers();
+        services.AddCorsPolicies();
+        services.AddGlobalRateLimiter();
+    }
+
     public static void InjectHandlers(this IServiceCollection services)
     {
         var handlers = typeof(Program).Assembly
--- MixxFit.API/Program.cs
+++ MixxFit.API/Program.cs
@@ -1,26 +1,7 @@
 using System.Security.Claims;
-using System.Text;
-using System.Threading.RateLimiting;
-using CloudinaryDotNet;
-using FluentValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using MixxFit.API.Common.Interfaces;
-using MixxFit.API.Domain.Entities;
-using MixxFit.API.Features.Workouts.CreateWorkout;
-using MixxFit.API.Infrastructure.Cloudinary;
-using MixxFit.API.Infrastructure.Cors;
-using MixxFit.API.Infrastructure.Exceptions;
 using MixxFit.API.Infrastructure.Extensions;
 using MixxFit.API.Infrastructure.Hangfire;
-using MixxFit.API.Infrastructure.Persistence;
-using MixxFit.API.Infrastructure.RateLimiting;
-using MixxFit.API.Infrastructure.Security;
-using MixxFit.API.Infrastructure.Storage;
 using Scalar.AspNetCore;
 using Serilog;
 
@@ -28,23 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, config) => config.ReadFrom.Configuration(context.Configuration));
 
-builder.Services.AddPersistence(builder.Configuration);
-builder.Services.AddSecurity(builder.Configuration);
-
-
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddProblemDetails();
-builder.Services.AddValidatorsFromAssemblyContaining<Program>(filter: descriptor => descriptor.ValidatorType != typeof(SetEntryValidator));
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddOpenApi();
-
-builder.Services.AddCloudinary(builder.Configuration);
-
-builder.Services.InjectHandlers();
-
-builder.Services.AddCorsPolicies();
-
-builder.Services.AddGlobalRateLimiter();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
@@ -52,9 +17,6 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
     options.KnownIPNetworks.Clear();
     options.KnownProxies.Clear();
 });
-
-builder.Services.AddHangfireSetup(builder.Configuration);
-builder.Services.AddRecurringJobs();
 
 var app = builder.Build();
 

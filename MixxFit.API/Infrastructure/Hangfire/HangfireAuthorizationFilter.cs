ff26a1801fd7065348faad3806ebb5beaf591c18
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-04-11 12:00:53 +0200

    feat(filters): Add hangfire filters that will be used for accessing the hangfire dashboard based on the application's environment type

---
+++ MixxFit.API/Infrastructure/Hangfire/HangfireAuthorizationFilter.cs
@@ -0,0 +1,38 @@
+﻿using Hangfire.Dashboard;
+using System.Text;
+
+namespace MixxFit.API.Infrastructure.Hangfire
+{
+    public class HangfireAuthorizationFilter(string user, string pass) : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            var auth = httpContext.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (auth is null || !auth.StartsWith("Basic "))
+            {
+                Challenge(httpContext);
+                return false;
+            }
+
+            var encodedCredentials = auth.Substring("Basic ".Length).Trim();
+            var decodedCredentials = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCredentials));
+            var parts = decodedCredentials.Split(':', 2);
+
+            if (parts.Length != 2 || parts[0] != user || parts[1] != pass)
+            {
+                Challenge(httpContext);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Challenge(HttpContext httpContext)
+        {
+            httpContext.Response.StatusCode = 401;
+            httpContext.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Hangfire Dashboard\"";
+        }
+    }
+}

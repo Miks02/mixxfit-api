843bcc5f5d5f6030f9113b54c45b74d988b6b7a0
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-04-25 09:24:59 +0200

    feat(dashboard): Add workout streak tracking to Dashboard API
    - Replaced `WaterIntake` and `AverageSleep` with `WorkoutStreak` in `GetDashboardResponse`.
    - Implemented `CalculateWorkoutStreakAsync` to compute workout streaks based on user data.

--- MixxFit.API/Features/Dashboard/GetDashboard/GetDashboardHandler.cs
+++ MixxFit.API/Features/Dashboard/GetDashboard/GetDashboardHandler.cs
@@ -17,7 +17,8 @@ public class GetDashboardHandler(AppDbContext context) : IHandler
         return new GetDashboardResponse
         {
             LastWorkoutDate = lastWorkoutDate,
-            RecentWorkouts = recentWorkouts
+            RecentWorkouts = recentWorkouts,
+            WorkoutStreak = await CalculateWorkoutStreakAsync(userId, cancellationToken)
         };
     }
     
@@ -41,7 +42,7 @@ public class GetDashboardHandler(AppDbContext context) : IHandler
             .Where(w => w.UserId == userId)
             .OrderByDescending(w => w.WorkoutDate)
             .Take(itemsToTake)
-            .Select(w => new RecentWorkoutDto()
+            .Select(w => new RecentWorkoutDto
             {
                 Id = w.Id,
                 Name = w.Name,
@@ -54,4 +55,41 @@ public class GetDashboardHandler(AppDbContext context) : IHandler
             })
             .ToListAsync(cancellationToken);
     }
+    
+    private async Task<int> CalculateWorkoutStreakAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        var workoutDates = await context.Workouts
+            .Where(u => u.UserId == userId)
+            .Select(w => w.WorkoutDate)
+            .OrderByDescending(d => d)
+            .Distinct()
+            .ToListAsync(cancellationToken);
+
+        if (workoutDates.Count == 0)
+            return 0;
+        
+        var today = DateTime.UtcNow;
+        var startDate = workoutDates[0] == today 
+            ? today 
+            : today.AddDays(-1);
+        
+        if(startDate > workoutDates[0])
+            return 0;
+        
+        var streakDays = 0;
+
+        foreach (var workout in workoutDates.Take(5))
+        {
+            if (workout == startDate.AddDays(-streakDays))
+            {
+                streakDays++;
+                continue;
+            }
+
+            break;
+        }
+
+        return streakDays;
+    }
+    
 }
--- MixxFit.API/Features/Dashboard/GetDashboard/GetDashboardResponse.cs
+++ MixxFit.API/Features/Dashboard/GetDashboard/GetDashboardResponse.cs
@@ -3,8 +3,7 @@ namespace MixxFit.API.Features.Dashboard.GetDashboard;
 public record GetDashboardResponse
 {
     public double DailyCalories { get; init; }
-    public double WaterIntake { get; init; }
-    public double AverageSleep { get; init; }
+    public int WorkoutStreak { get; init; }
     public DateTime? LastWorkoutDate { get; init; }
     public IReadOnlyList<RecentWorkoutDto> RecentWorkouts { get; init; } = new List<RecentWorkoutDto>();
 }

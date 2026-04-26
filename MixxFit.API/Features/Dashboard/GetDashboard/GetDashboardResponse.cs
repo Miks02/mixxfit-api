950d641ebe872a5021dc9a5279ffed0cc50c0573
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-04-26 10:16:54 +0200

    refactor(dashboard): Remove DailyCalories from GetDashboardResponse

--- MixxFit.API/Features/Dashboard/GetDashboard/GetDashboardResponse.cs
+++ MixxFit.API/Features/Dashboard/GetDashboard/GetDashboardResponse.cs
@@ -2,7 +2,6 @@ namespace MixxFit.API.Features.Dashboard.GetDashboard;
 
 public record GetDashboardResponse
 {
-    public double DailyCalories { get; init; }
     public int WorkoutStreak { get; init; }
     public DateTime? LastWorkoutDate { get; init; }
     public IReadOnlyList<RecentWorkoutDto> RecentWorkouts { get; init; } = new List<RecentWorkoutDto>();

ca083a45b31f488ad1c480f0539a5dc73f8893e5
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-04-27 03:14:04 +0200

    chore(migrations): Add migrations for removing calorie entries

---
+++ MixxFit.API/Infrastructure/Persistence/Migrations/20260427011309_RemoveCalorieEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MixxFit.API.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCalorieEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalorieEntries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalorieEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Calories = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    Description = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalorieEntries", x => x.Id);
+                    table.CheckConstraint("CK_CalorieEntrys_Calories_Positive", "\"Calories\" > 0");
+                    table.ForeignKey(
+                        name: "FK_CalorieEntries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalorieEntries_UserId",
+                table: "CalorieEntries",
+                column: "UserId");
+        }
+    }
+}

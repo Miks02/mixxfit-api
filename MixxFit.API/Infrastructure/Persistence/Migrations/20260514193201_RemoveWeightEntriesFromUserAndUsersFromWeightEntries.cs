a737da5e3acc59da0c82b93a97a71d7667879801
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-14 21:33:42 +0200

    chore(migration): Commit missing migration

---
+++ MixxFit.API/Infrastructure/Persistence/Migrations/20260514193201_RemoveWeightEntriesFromUserAndUsersFromWeightEntries.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MixxFit.API.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveWeightEntriesFromUserAndUsersFromWeightEntries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeightEntries_FitnessProfiles_FitnessProfileId",
+                table: "WeightEntries");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeightEntries_Users_UserId",
+                table: "WeightEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WeightEntries_UserId",
+                table: "WeightEntries");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "WeightEntries");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeightEntries_FitnessProfiles_FitnessProfileId",
+                table: "WeightEntries",
+                column: "FitnessProfileId",
+                principalTable: "FitnessProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeightEntries_FitnessProfiles_FitnessProfileId",
+                table: "WeightEntries");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "WeightEntries",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeightEntries_UserId",
+                table: "WeightEntries",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeightEntries_FitnessProfiles_FitnessProfileId",
+                table: "WeightEntries",
+                column: "FitnessProfileId",
+                principalTable: "FitnessProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeightEntries_Users_UserId",
+                table: "WeightEntries",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

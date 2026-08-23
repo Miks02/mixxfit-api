bbb8761e245ef95e28310cf432f500cd93697cc6
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-23 10:14:39 +0200

    feat(security): Update CookieProvider to use configuration for refresh token expiration and remove GetUserIp method

--- MixxFit.API/Common/Interfaces/ICookieProvider.cs
+++ MixxFit.API/Common/Interfaces/ICookieProvider.cs
@@ -3,7 +3,6 @@ namespace MixxFit.API.Common.Interfaces;
 public interface ICookieProvider
 {
     public string GetRefreshTokenCookie();
-    public string GetUserIp();
     public void SetRefreshTokenCookie(string refreshToken);
     public void DeleteRefreshTokenCookie();
 }

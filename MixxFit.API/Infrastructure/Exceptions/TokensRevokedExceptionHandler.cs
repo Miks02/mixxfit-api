1c9bc757a63daf49d4391f85e5149410c8347eae
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-23 10:14:17 +0200

    feat(exception): Add handlers for SecurityDbUpdateException and AllTokensRevokedException

---
+++ MixxFit.API/Infrastructure/Exceptions/TokensRevokedExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MixxFit.API.Infrastructure.Exceptions
+{
+    public class TokensRevokedExceptionHandler(
+        ILogger<TokensRevokedExceptionHandler> logger,
+        IProblemDetailsService problemDetailsService) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is not AllTokensRevokedException)
+                return false;
+
+            var userIp = httpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP";
+            var traceId = httpContext.TraceIdentifier;
+
+            httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+
+            logger.LogWarning("Security breach detected! All user's sessions have been revoked. User IP: {UserIp} . TraceID: {TraceId}", userIp, traceId);
+            
+            httpContext.Response.Cookies.Delete("refreshToken", new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                SameSite = SameSiteMode.None,
+                Path = "/"
+            });
+            
+            return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = httpContext,
+                Exception = exception,
+                ProblemDetails = new ProblemDetails
+                {
+                    Type = "urn:mixxfit-api:error:tokens-revoked",
+                    Title = "Unauthorized",
+                    Detail = "All client sessions have been revoked due to a possible security breach."
+                }
+            });
+
+        }
+    }
+}

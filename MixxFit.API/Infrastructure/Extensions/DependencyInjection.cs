3fb75f5ace57daaedc507fa46bffda41fff748e2
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-23 11:40:00 +0200

    feat(background): Add CleanExpiredTokensJob to periodically remove expired refresh tokens

--- MixxFit.API/Infrastructure/Extensions/DependencyInjection.cs
+++ MixxFit.API/Infrastructure/Extensions/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using FluentValidation;
 using MixxFit.API.Common.Interfaces;
+using MixxFit.API.Features.Exercises.CleanUpExercisesJob;
 using MixxFit.API.Features.Workouts.CreateWorkout;
+using MixxFit.API.Infrastructure.BackgroundServices;
 using MixxFit.API.Infrastructure.Cloudinary;
 using MixxFit.API.Infrastructure.Cors;
 using MixxFit.API.Infrastructure.Exceptions;
@@ -16,6 +18,7 @@ public static class DependencyInjection
     {
         services.AddPersistence(configuration);
         services.AddSecurity(configuration);
+        services.AddHostedService<CleanExpiredTokensJob>();
         services.AddHttpContextAccessor();
         services.AddCloudinary(configuration);
         services.AddProblemDetails();
